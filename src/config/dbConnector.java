1946515283ec650078ecead087c903eccc854d07
yaelpongs <yaelyaelyael18@gmail.com>
2024-03-26 13:46:00 +0800

    first commit

---
+++ src/config/dbConnector.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author SCC
+ */
+public class dbConnector {
+    
+    private Connection connect;
+    
+     public dbConnector(){
+            try{
+                connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/testapp26", "root", "");
+            }catch(SQLException ex){
+                    System.out.println("Can't connect to database: "+ex.getMessage());
+            }
+        }
+     
+     //Function to retrieve data
+        public ResultSet getData(String sql) throws SQLException{
+            Statement stmt = connect.createStatement();
+            ResultSet rst = stmt.executeQuery(sql);
+            return rst;
+        }
+}
